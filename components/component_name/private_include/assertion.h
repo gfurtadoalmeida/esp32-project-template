cfe7faee678bf20b4e528de262662104bd3473f1
gfurtadoalmeida <27151609+gfurtadoalmeida@users.noreply.github.com>
2023-04-21 09:59:21 -0300

    Refactored assertion

--- components/component_name/private_include/assertion.h
+++ components/component_name/private_include/assertion.h
@@ -12,14 +12,14 @@ extern "C"
  * @brief Returns a given value if the condition passed is false.
  *
  * @param condition Condition to evaluate.
- * @param return_value Value to be returned if the condition is false.
  * @param message Message to be logged if the condition is false.
+ * @param return_value Value to be returned if the condition is false.
  *
  * @return If the condition is false the method will return what was
  * passed on @ref return_value parameter, otherwise the method will
  * continue.
  */
-#define CPM_CHECK(condition, return_value, message)              \
+#define CMP_CHECK(condition, message, return_value)              \
     if (!(condition))                                            \
     {                                                            \
         CMP_LOGE("%s(%d): %s", __FUNCTION__, __LINE__, message); \
